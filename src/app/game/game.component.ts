9f839cf011f689f13d7bb2cdb91bac80295772da
Parker <parkerngore@gmail.com>
2020-12-26 12:19:07 -0700

    in progress of implementing minimax algorithm

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,3 +1,4 @@
+import { identifierModuleUrl } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,40 +8,209 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
   gameBoard = [
-    ['x', 'o', 'x'],
-    ['o', 'x', 'o'],
-    ['x', 'o', 'x'],
+    ['X', 'O', 'X'],
+    ['O', 'X', 'O'],
+    ['X', 'O', 'X'],
   ];
 
-  playerOne = 'x';
-  playerTwo = 'o';
+  playerOne = 'X';
+  playerTwo = 'O';
 
-  playerTurn = 'x';
+  isComputer = true;
+  computer = this.playerOne;
+  player = this.playerTwo;
 
-  placeMarker(event) {
+  playerTurn = 'X';
+
+  scores = {};
+
+  startGame() {
+    this.playerTurn = 'X';
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        this.gameBoard[i][j] = '';
+      }
+    }
+
+    if (this.isComputer && this.computer === 'X') {
+      this.scores = {
+        X: 1,
+        O: -1,
+        tie: 0,
+      };
+      this.computerMove();
+    } else if (this.isComputer && this.computer === 'O') {
+      return alert('Computer is O');
+    } else {
+    }
+  }
+
+  computerMove() {
+    console.log('yes');
+    let bestScore = -Infinity;
+    let move;
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (this.gameBoard[i][j] === '') {
+          this.gameBoard[i][j] = this.computer;
+
+          let score = this.minimax(this.gameBoard, 0, false);
+          this.gameBoard[i][j] = '';
+          if (score > bestScore) {
+            bestScore = score;
+            move = { i, j };
+          }
+        }
+      }
+    }
+    this.gameBoard[move.i][move.j] = this.computer;
+    if (this.computer === 'X') {
+      this.playerTurn = 'O';
+    } else if (this.computer === 'O') {
+      this.playerTurn = 'X';
+    }
+  }
+
+  minimax(gameBoard, depth, isMaximizing) {
+    let result = this.isGameOver();
+    if (result !== null) {
+      return this.scores[result];
+    }
+    if (isMaximizing) {
+      let bestScore = -Infinity;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (gameBoard[i][j] === '') {
+            gameBoard[i][j] = this.computer;
+            let score = this.minimax(gameBoard, depth + 1, false);
+            gameBoard[i][j] = '';
+            bestScore = Math.max(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (gameBoard[i][j] === '') {
+            gameBoard[i][j] = this.player;
+            let score = this.minimax(gameBoard, depth, true);
+            gameBoard[i][j] = '';
+            bestScore = Math.min(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    }
+  }
+
+  nextTurn(event) {
     const idString = event.currentTarget.id;
     const rowId = idString.charAt(3);
     const squareId = idString.charAt(10);
 
+    this.placeMarker(rowId, squareId);
+  }
+
+  placeMarker(rowId, squareId) {
     if (this.gameBoard[rowId][squareId] !== '') {
       alert('that spot is taken!');
       return;
     }
 
-    if (this.playerTurn === 'x') {
-      this.gameBoard[rowId][squareId] = 'x';
-      this.playerTurn = 'o';
+    if (this.playerTurn === 'X') {
+      this.gameBoard[rowId][squareId] = 'X';
+      this.playerTurn = 'O';
     } else {
-      this.gameBoard[rowId][squareId] = 'o';
-      this.playerTurn = 'x';
+      this.gameBoard[rowId][squareId] = 'O';
+      this.playerTurn = 'X';
+    }
+
+    this.isGameOver();
+  }
+
+  isMarked(i, j) {
+    const row = i;
+    const square = j;
+
+    if (this.gameBoard[row][square] !== '') {
+      return true;
+    }
+    if (
+      this.gameBoard[row][square] === 'X' ||
+      this.gameBoard[row][square] === 'O'
+    ) {
+      return false;
     }
   }
 
-  startGame() {
+  isWinner(a, b, c) {
+    return a === b && b === c && a != '';
+  }
+
+  isGameOver() {
+    let winner = null;
+
+    for (let i = 0; i < 3; i++) {
+      if (
+        this.isWinner(
+          this.gameBoard[i][0],
+          this.gameBoard[i][1],
+          this.gameBoard[i][2]
+        )
+      ) {
+        winner = this.gameBoard[i][0];
+      }
+    }
+
+    for (let i = 0; i < 3; i++) {
+      if (
+        this.isWinner(
+          this.gameBoard[0][i],
+          this.gameBoard[1][i],
+          this.gameBoard[2][i]
+        )
+      ) {
+        winner = this.gameBoard[0][i];
+      }
+    }
+
+    //checking for diagnal winner
+    if (
+      this.isWinner(
+        this.gameBoard[0][0],
+        this.gameBoard[1][1],
+        this.gameBoard[2][2]
+      )
+    ) {
+      winner = this.gameBoard[0][0];
+    }
+    if (
+      this.isWinner(
+        this.gameBoard[2][0],
+        this.gameBoard[1][1],
+        this.gameBoard[0][2]
+      )
+    ) {
+      winner = this.gameBoard[2][0];
+    }
+
+    let openSpots = 0;
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        this.gameBoard[i][j] = '';
+        if (this.gameBoard[i][j] === '') {
+          openSpots++;
+        }
       }
+    }
+
+    if (winner === null && openSpots === 0) {
+      return 'Draw';
+    } else {
+      return winner;
     }
   }
 
